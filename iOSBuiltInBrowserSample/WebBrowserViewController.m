83c036aed873e03948e03925ad88cba86a2e3d65
Tomohiko Himura <eiel.hal@gmail.com>
2013-02-18 00:20:47 +0900

    最近は syncethize かかないのが流行り?
    僕は i_hogehoge とかにして補完しやすくしたりするけど。

--- iOSBuiltInBrowserSample/WebBrowserViewController.m
+++ iOSBuiltInBrowserSample/WebBrowserViewController.m
@@ -10,12 +10,6 @@
 
 @implementation WebBrowserViewController
 
-@synthesize webView;
-@synthesize url_;
-@synthesize backBar;
-@synthesize backBtn;
-@synthesize goBtn;
-
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -42,15 +36,15 @@
     self.navigationController.navigationBarHidden = YES;
 
     // 戻るボタンを作成（左三角がないので反転）
-    backBar.transform = CGAffineTransformMakeScale(-1,1);
+    _backBar.transform = CGAffineTransformMakeScale(-1,1);
     //戻ると進むを非活性
-    backBtn.enabled = NO;
-    goBtn.enabled = NO;
+    _backBtn.enabled = NO;
+    _goBtn.enabled = NO;
 
     // インジケーターの表示
     [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
     // URLの読み込み
-    [webView loadRequest:[NSURLRequest requestWithURL:url_]];
+    [_webView loadRequest:[NSURLRequest requestWithURL:_url_]];
 }
 
 - (void)viewDidUnload
@@ -77,8 +71,8 @@
     // インジケーターの非表示
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
     // ボタンの活性・非活性を切り替え
-    backBtn.enabled = webView_.canGoBack;
-    goBtn.enabled = webView_.canGoForward;
+    _backBtn.enabled = webView_.canGoBack;
+    _goBtn.enabled = webView_.canGoForward;
 }
 
 #pragma mark - actions
@@ -92,7 +86,7 @@
     // インジケーターの非表示
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
     // webViewのデリゲート解放
-    webView.delegate = nil;
+    _webView.delegate = nil;
     
     // 現在のViewを閉じる
     [self dismissViewControllerAnimated:YES completion:nil];
@@ -102,23 +96,23 @@
 -(IBAction)reloadUrl:(id)sender {
     // インジケーターの表示
     [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
-    [webView reload];
+    [_webView reload];
 }
 
 // 戻るボタン
 -(IBAction)back:(id)sender{
-    [webView goBack];
+    [_webView goBack];
 }
 
 // 進むボタン
 -(IBAction)forward:(id)sender {
-    [webView goForward];
+    [_webView goForward];
 }
 
 // サファリで開く
 -(IBAction)openSafari:(id)sender {
     // 現在のURLを取得
-    NSString* url = [webView stringByEvaluatingJavaScriptFromString:@"document.URL"];
+    NSString* url = [_webView stringByEvaluatingJavaScriptFromString:@"document.URL"];
     // サファリでオープン
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]];
 }
